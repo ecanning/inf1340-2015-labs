67dbcf6737fe0fad55c1cfda9f47b44ba49734ad
ecanning <erin.canning@mail.utoronto.ca>
2015-10-29 11:28:08 -0400

    test cases written, all pass

--- test_lab3.py
+++ test_lab3.py
@@ -12,6 +12,7 @@ from lab3 import days_in_month
 MONTHS_WITH_31 = ["January", "March", "May", "July", "August", "October", "December"]
 MONTHS_WITH_30 = ["April", "June", "September", "November"]
 MONTHS_WITH_28_or_29 = ["February"]
+VALUE_ERROR_TEST_LIST = ["256", "tomorrow", 13]
 
 
 def test_months_with_31():
@@ -24,13 +25,37 @@ def test_months_with_31():
 # Write a test function for the months with 30 days
 
 
+def test_months_with_30():
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
+
 # Write a test function for the months with 28 or 29 days
 
 
+def test_months_with_28_or_29():
+    for item in MONTHS_WITH_28_or_29:
+        assert days_in_month(item) == "28 or 29"
+
 # Write a test function for months that are not capitalized properly
 # Hint: use the lower() string method
+
+
+def test_lower_case_words():
+    for item in MONTHS_WITH_31:
+        item = item.lower()
+        assert days_in_month(item) == 31
+
 
 # Write a test function for unexpected input
 # Hint: use a try/except block to deal with the exception
 # Hint: use data types other than strings as input
 
+
+def test_value_error():
+    try:
+        for item in VALUE_ERROR_TEST_LIST:
+            days_in_month(item)
+    except TypeError:
+        assert True
+    except ValueError:
+        assert True
